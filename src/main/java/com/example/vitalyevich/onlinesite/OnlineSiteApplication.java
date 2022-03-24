2782fa642be744c5e3c8e45a5cf6e4382847ae66
vitalyevich <maksim_2411@mail.ru>
2022-03-24 13:03:53 +0300

    add project

---
+++ src/main/java/com/example/vitalyevich/onlinesite/OnlineSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.vitalyevich.onlinesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vitalyevich/onlinesite/OnlineSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitalyevich.onlinesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
